8635a501b4066bc0d8d6391a38631b231c6f89e1
noveralexander@gmail.com <you@example.com>
2022-10-24 13:19:13 +0700

    add datatables and jquery

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,18 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    
+    @if(isset($extra['css']))
+    @foreach($extra['css'] as $key => $item)
+      <link rel="stylesheet" href="{{$item}}">
+    @endforeach
+    @endif
+
+    @if(isset($extra['js']))
+    @foreach($extra['js'] as $key => $item)
+      <script src="{{$item}}"></script>
+    @endforeach
+    @endif
   </head>
   <body>
     @if(Session::has('id'))
