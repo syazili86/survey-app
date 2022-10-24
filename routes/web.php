605b9b9d741a7857a8d2bae370ed3efa449ba3b8
noveralexander@gmail.com <you@example.com>
2022-10-24 13:18:57 +0700

    add middleware by user role

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Enroll;
+use App\Http\Controllers\Report;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,7 +24,12 @@ Route::get('/', [LoginController::class, 'index']);
 Route::post('/', [LoginController::class, 'doAuth']);
 Route::get('/logout', [LoginController::class, 'logout']);
 
-Route::group(['middleware'=>['CustomAuth']], function(){
+Route::group(['middleware'=>['StudentAuth']], function(){
   Route::get('/enrol', [Enroll::class, 'index']);
   Route::post('/enrol', [Enroll::class, 'doSurvey']);
+});
+
+Route::group(['middleware'=>['AdminAuth']], function(){
+  Route::get('/report', [Report::class, 'index']);
+  Route::get('/report/{id}/detail', [Report::class, 'detail']);
 });
