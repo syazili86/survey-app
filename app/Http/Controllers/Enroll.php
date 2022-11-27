83347b53121de19443ef75793a52f5b948d6d261
chiprodoy <chiprodoy@gmail.com>
2022-11-28 06:09:43 +0700

    fixing survey session

--- app/Http/Controllers/Enroll.php
+++ app/Http/Controllers/Enroll.php
@@ -17,7 +17,7 @@ class Enroll extends Controller
     public function index(Request $request){
         $request->session()->remove('flashMsg');
 
-        $session = SurveySession::where('active', true)->where('end', '>', Carbon::now())->first();
+        $session = SurveySession::where('active', true)->where('start', '<', Carbon::now())->first();
 
         if ($session == null) {
             $request->session()->put('flashMsg', "Sesi survey belum dibuka, silahkan hubungi pihak yang bersangkutan untuk jadwal");
